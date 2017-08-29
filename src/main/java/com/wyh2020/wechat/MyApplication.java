fce53f74d3e21ffb71aa6c7d827181ac58afc41e
of1410 <wangyonghua@qianmi.com>
2017-08-29 14:58:05 +0800

    init

---
+++ src/main/java/com/wyh2020/wechat/MyApplication.java
@@ -0,0 +1,16 @@
+package com.wyh2020.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with wyh.
+ * Date: 2017/8/28
+ * Time: 下午3:28
+ */
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
---
+++ src/main/java/com/wyh2020/wechat/handler/ScanHandler.java
@@ -0,0 +1,8 @@
+package com.wyh2020.wechat.handler;
+
+/**
+ * @author Binary Wang(https://github.com/binarywang)
+ */
+public abstract class ScanHandler extends AbstractHandler {
+
+}
